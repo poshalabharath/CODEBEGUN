fb86b7dcaa40c9e1ab8e0a26ec83cc90983457c5
poshala bharath <148369337+poshalabharath@users.noreply.github.com>
2024-11-15 21:27:15 +0530

    Add files via upload

---
+++ javaScript2/script.js
@@ -0,0 +1,39 @@
+// var a={
+//     name: "tata",
+//     model : 2023,
+//     cost :24000000,
+//     color:"yellow",
+
+// }
+// document.write(a.name);
+// document.write(a.model);
+// document.write(a.cost);
+// document.write(a.color);
+
+
+
+//Es5
+// function addition() {
+//     let a = 10;
+//     let b = 10;
+//     let c = a + b;
+//     alert(c);
+//     console.log("You clicked here", c);
+// }
+// setTimeout(addition(), 3000);
+
+
+
+// Es6
+// const a= () =>{
+//      return (
+//         console.log("welcome to the vaagdevi college guys")
+//      )
+// }
+// a()
+
+// Es7
+
+// const a=()=>console.log("hello guys" );
+// a()
+
